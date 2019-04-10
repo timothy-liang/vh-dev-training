aee7f527aec33b3b2c3168aa03d4ae6ac8ad446f
Tim Liang <timothy.liang@vanderbilt.edu>
2019-04-10 18:22:43 -0500

    Complete HW1 Exercises

--- week1/hw/hw.js
+++ week1/hw/hw.js
@@ -1,13 +1,52 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
 exports.__esModule = true;
+/* eslint-disable */ // just to prevent it from annoying us forever
 var node_fetch_1 = require("node-fetch");
-// to call this function, you'll need to do two things
-// 1st, your function must have the keyword "async" before the parameters
-// 2nd when you call it, you must preface it with "await"
-// like this: await getDataFromAPI(url)
+// TODO: make typescript declaration for module
+var HttpsProxyAgent = require('https-proxy-agent');
 var getDataFromAPI = function (url) {
+    var req = {};
+    if (process.env.HTTP_PROXY) {
+        req.agent = new HttpsProxyAgent(process.env.HTTP_PROXY);
+    }
     return new Promise(function (res, rej) {
-        node_fetch_1["default"](url)
+        node_fetch_1["default"](url, req)
             .then(function (data) { return data.json(); })
             .then(function (json) {
             // the json variable here is just the stuff you see in the browser
@@ -15,12 +54,93 @@ var getDataFromAPI = function (url) {
         })["catch"](function (err) { return rej(err); });
     });
 };
+// all these parantheses and braces creates and instantly calls a function.
+// This is known as an Instantly Invoked Function Expression (IIFE).
+// It has a lot of uses but in this case, since await can only be used in async functions, it's needed
+// (async () => {
+// 	console.log(await getDataFromAPI('https://spot.benc.me/?time=1549939921')); // sample call - delete before you submit
+// })();
 // write your homework here
-// put all this in your "objectively" function after you make it
-var whatAmI = { 0: 'A', 1: 'B', 2: 'C', 3: 'D' };
-// put this stuff in your "awry" function after you make it
-var docTester = [];
-// fill docTester with 1000 random integers between 0 and 99
-for (var i = 0; i < 1000; i++)
-    docTester[i] = Math.floor(Math.random() * 100);
-console.log(docTester);
+var objectively = function () { return __awaiter(_this, void 0, void 0, function () {
+    var whatAmI, getNthByUnique, _a, _b;
+    var _this = this;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                console.log(console);
+                whatAmI = { 0: 'A', 1: 'B', 2: 'C', 3: 'D' };
+                getNthByUnique = function (n) { return __awaiter(_this, void 0, void 0, function () {
+                    var data;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4 /*yield*/, getDataFromAPI('https://spot.benc.me/?time=1549939921')];
+                            case 1:
+                                data = _a.sent();
+                                return [2 /*return*/, data.sort(function (a, b) {
+                                        var uniqueDifference = b.unique - a.unique;
+                                        return uniqueDifference != 0 ? uniqueDifference : b.score - a.score;
+                                    })[n - 1]];
+                        }
+                    });
+                }); };
+                _b = (_a = console).log;
+                return [4 /*yield*/, getNthByUnique(3)];
+            case 1:
+                _b.apply(_a, [_c.sent()]);
+                return [2 /*return*/];
+        }
+    });
+}); };
+(function () { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
+    switch (_a.label) {
+        case 0: return [4 /*yield*/, objectively()];
+        case 1: return [2 /*return*/, _a.sent()];
+    }
+}); }); })();
+var awry = function () { return __awaiter(_this, void 0, void 0, function () {
+    var docTester, i, docTesterIndex, getNthByUniqueFiltered, _a, _b;
+    var _this = this;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0:
+                docTester = [];
+                // fill docTester with 1000 random integers between 0 and 99
+                for (i = 0; i < 1000; i++)
+                    docTester[i] = Math.floor(Math.random() * 100);
+                console.log(docTester); // uncomment this to see the array logged
+                docTesterIndex = [];
+                docTester.forEach(function (element, index) { return (docTesterIndex[index] = element + index); });
+                console.log(docTesterIndex);
+                docTester.map(function (element, index) { return (docTesterIndex[index] = element + index); });
+                console.log(docTesterIndex);
+                console.log(docTesterIndex.reduce(function (accumulator, element) { return (accumulator += element); }, 0));
+                getNthByUniqueFiltered = function (n) { return __awaiter(_this, void 0, void 0, function () {
+                    var data;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4 /*yield*/, getDataFromAPI('https://spot.benc.me/?time=1549939921')];
+                            case 1:
+                                data = _a.sent();
+                                return [2 /*return*/, data
+                                        .filter(function (element) { return element.spotted == 0 || element.spotted >= 3; })
+                                        .sort(function (a, b) {
+                                        var uniqueDifference = b.unique - a.unique;
+                                        return uniqueDifference != 0 ? uniqueDifference : b.score - a.score;
+                                    })[n - 1]];
+                        }
+                    });
+                }); };
+                _b = (_a = console).log;
+                return [4 /*yield*/, getNthByUniqueFiltered(3)];
+            case 1:
+                _b.apply(_a, [_c.sent()]);
+                return [2 /*return*/];
+        }
+    });
+}); };
+(function () { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
+    switch (_a.label) {
+        case 0: return [4 /*yield*/, awry()];
+        case 1: return [2 /*return*/, _a.sent()];
+    }
+}); }); })();
--- week1/hw/hw.ts
+++ week1/hw/hw.ts
@@ -1,13 +1,31 @@
 /* eslint-disable */ // just to prevent it from annoying us forever
-import fetch from 'node-fetch';
+import fetch, { RequestInit } from 'node-fetch';
+
+// TODO: make typescript declaration for module
+let HttpsProxyAgent = require('https-proxy-agent');
 
 // to call this function, you'll need to do two things
 // 1st, the function that is called in must have the keyword "async" before the parameters
 // 2nd when you call it, you must preface it with "await"
 // like in the below example
-const getDataFromAPI = (url: string) =>
-	new Promise((res, rej) => {
-		fetch(url)
+
+interface HackerRecord {
+	name: string;
+	score: number;
+	spots: number;
+	spotted: number;
+	invalidated: number;
+	unique: number;
+	id: string;
+}
+
+const getDataFromAPI = (url: string): Promise<HackerRecord[]> => {
+	let req: RequestInit = {};
+	if (process.env.HTTP_PROXY) {
+		req.agent = new HttpsProxyAgent(process.env.HTTP_PROXY);
+	}
+	return new Promise((res, rej) => {
+		fetch(url, req)
 			.then(data => data.json())
 			.then(json => {
 				// the json variable here is just the stuff you see in the browser
@@ -15,21 +33,62 @@ const getDataFromAPI = (url: string) =>
 			})
 			.catch(err => rej(err));
 	});
+};
 
 // all these parantheses and braces creates and instantly calls a function.
 // This is known as an Instantly Invoked Function Expression (IIFE).
 // It has a lot of uses but in this case, since await can only be used in async functions, it's needed
-(async () => {
-	console.log(await getDataFromAPI('https://spot.benc.me/?time=1549939921')); // sample call - delete before you submit
-})();
+// (async () => {
+// 	console.log(await getDataFromAPI('https://spot.benc.me/?time=1549939921')); // sample call - delete before you submit
+// })();
 
 // write your homework here
+const objectively = async () => {
+	console.log(console);
 
-// put all this in your "objectively" function after you make it
-const whatAmI = { 0: 'A', 1: 'B', 2: 'C', 3: 'D' };
+	// put all this in your "objectively" function after you make it
+	const whatAmI = { 0: 'A', 1: 'B', 2: 'C', 3: 'D' };
 
-// put this stuff in your "awry" function after you make it
-const docTester = [];
-// fill docTester with 1000 random integers between 0 and 99
-for (let i = 0; i < 1000; i++) docTester[i] = Math.floor(Math.random() * 100);
-// console.log(docTester); // uncomment this to see the array logged
+	const getNthByUnique = async (n: number): Promise<HackerRecord> => {
+		let data = await getDataFromAPI('https://spot.benc.me/?time=1549939921');
+		return data.sort((a, b) => {
+			let uniqueDifference = b.unique - a.unique;
+			return uniqueDifference != 0 ? uniqueDifference : b.score - a.score;
+		})[n - 1];
+	};
+
+	console.log(await getNthByUnique(3));
+};
+
+(async () => await objectively())();
+
+const awry = async () => {
+	// put this stuff in your "awry" function after you make it
+	const docTester = [];
+	// fill docTester with 1000 random integers between 0 and 99
+	for (let i = 0; i < 1000; i++) docTester[i] = Math.floor(Math.random() * 100);
+	console.log(docTester); // uncomment this to see the array logged
+
+	let docTesterIndex: number[] = [];
+	docTester.forEach((element, index) => (docTesterIndex[index] = element + index));
+	console.log(docTesterIndex);
+
+	docTester.map((element, index) => (docTesterIndex[index] = element + index));
+	console.log(docTesterIndex);
+
+	console.log(docTesterIndex.reduce((accumulator, element) => (accumulator += element), 0));
+
+	const getNthByUniqueFiltered = async (n: number): Promise<HackerRecord> => {
+		let data = await getDataFromAPI('https://spot.benc.me/?time=1549939921');
+		return data
+			.filter(element => element.spotted == 0 || element.spotted >= 3)
+			.sort((a, b) => {
+				let uniqueDifference = b.unique - a.unique;
+				return uniqueDifference != 0 ? uniqueDifference : b.score - a.score;
+			})[n - 1];
+	};
+
+	console.log(await getNthByUniqueFiltered(3));
+};
+
+(async () => await awry())();
